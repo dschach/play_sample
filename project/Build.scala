e62b45a6c93af703408f397ebf9c0975c39ee9e0
Jeff Casimir <jeff@casimircreative.com>
2014-03-20 15:03:50 -0600

    Adding PGSQL support and a Procfile for Heroku

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,11 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      ebeanEnabled := false   
+      ebeanEnabled := false
     )
 
 }
-            
